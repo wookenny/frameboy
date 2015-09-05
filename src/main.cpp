37396e882786b0b79f217721cce33bac854ddc1e
wookenny <kenny@wook.de>
2015-09-05 14:21:29 +0200

    fixed some issues to make it compile on differnt machine

--- src/main.cpp
+++ src/main.cpp
@@ -21,7 +21,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QApplication::setGraphicsSystem("native");
+    //QApplication::setGraphicsSystem("native");
     MainWindow w;
     w.show();
 
